a9c9a2107504c4474dad71378074ddc52761bb60
Kaue Braga <kauebraga90@gmail.com>
2022-03-22 14:57:46 -0300

    update

--- 5.0-calculate_iscocrones.R
+++ 5.0-calculate_iscocrones.R
@@ -1,8 +1,9 @@
-options(java.parameters = '-Xmx10G')
+options(java.parameters = '-Xmx20G')
 library(r5r)
 library(data.table)
 library(dplyr)
 library(sf)
+library(mapview)
 
 # open scenario
 cenario1 <- st_read("../../data/smtr_malha_cicloviaria/4-osm_cenarios/osm_cenario1.gpkg") %>% mutate(cenario = "cenario1")
@@ -31,13 +32,66 @@ ttmatrix_bike <- r5r::travel_time_matrix(r5r_core = setup,
                                          max_trip_duration = 5)
 
 # trazer osm id para a ttmatrix
+setnames(ttmatrix_bike, old = "fromId", new = "id")
+ttmatrix_bike[, id := as.integer(id)]
+ttmatrix_bike[cenario_points_coords, on = "id",
+              c("osm_id") := i.osm_id]
+
+# bring coordinates
+ttmatrix_bike[points, on = c("toId" = "id"),
+              c("lon", "lat") := list(i.lon, i.lat)]
+
 
 
 # save
 fwrite(ttmatrix_bike, "../../data/smtr_malha_cicloviaria/ttmatrix_iso_bike.csv")
 
 
-# 300 metros de bike = 1.5 minutos
+# osm_id1 <- "15807931"
+# funcao para cada osm_id
+iso_osm_id <- function(osm_id1) {
+  
+  
+  # para cada pontinho de cada osm id
+  # list all pontos to osm id
+  points_osm_id <- ttmatrix_bike[osm_id == osm_id1]
+  points_osm_id <- st_as_sf(points_osm_id, coords = c("lon", "lat"), crs = 4326)
+  # mapview(points_osm_id)
+  # st_as_sfc(st_bbox(points_osm_id)) %>% mapview() + points_osm_id
+  iso <- st_as_sfc(st_bbox(points_osm_id), crs = 4326)
+  
+  # construct polygon after these points
+  
+  # para cada pontinho, pegar os 3 mais longes?
+  
+  # variables <- 1
+  # iso_pontinho <- function(variables) {
+  #   
+  #   points_osm_id_pontinho <- points_osm_id[id == variables]
+  #   iso_osm_pontinho <- sfheaders::sf_polygon(points_osm_id_pontinho, x = "lon", y= "lat")
+  #   mapview(iso_osm_pontinho)
+  #   
+  #   
+  #   library(stars)
+  #   iso_osm_pontinho_star <- stars::st_as_stars(iso_osm_pontinho)
+  #   st_contour(iso_osm_pontinho_star)
+  #   
+  #   iso_osm_pontinho <- st_as_sf(points_osm_id_pontinho, coords = c("lon", "lat"), crs = 4326)
+  #   mapview(iso_osm_pontinho)
+  #   iso_osm_pontinho_hux <- st_convex_hull(iso_osm_pontinho)
+  #   mapview(iso_osm_pontinho_hux)
+  #   
+  # }
+  
+}
+
+iso_todos <- lapply(unique(cenario_points_coords$osm_id), iso_osm_id)
+a <- do.call(c, iso_todos)
+b <- data.frame(osm_id = unique(cenario_points_coords$osm_id))
+st_geometry(b) <- a
+
+
+plot(b)
 
 # open malha
 cenario1 <- st_read("../../data/smtr_malha_cicloviaria/4-osm_cenarios/osm_cenario1.gpkg") %>% mutate(cenario = "cenario1")
