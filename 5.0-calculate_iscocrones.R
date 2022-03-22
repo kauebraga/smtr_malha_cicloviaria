37c392e81336fc5f0f61acb3ade2be7581474e49
Kaue Braga <kauebraga90@gmail.com>
2022-03-22 13:58:40 -0300

    update

--- 5.0-calculate_iscocrones.R
+++ 5.0-calculate_iscocrones.R
@@ -1,21 +1,37 @@
-options(java.parameters = '-Xmx20G')
+options(java.parameters = '-Xmx10G')
 library(r5r)
 library(data.table)
 library(dplyr)
 library(sf)
 
+# open scenario
+cenario1 <- st_read("../../data/smtr_malha_cicloviaria/4-osm_cenarios/osm_cenario1.gpkg") %>% mutate(cenario = "cenario1")
+
+# break scenario in points every 20 meters
+# standardize shape resolution - at least every 20 meters
+cenario_points <- st_segmentize(cenario1, dfMaxLength = 20)
+# shapes_linhas_filter <- sfheaders::sf_cast(shapes_linhas_filter, "POINT")
+# transform to lon lat - these points will be the origins
+cenario_points_coords <- sfheaders::sf_to_df(cenario_points, fill = TRUE) %>% 
+  mutate(id = 1:n()) %>%
+  select(id, osm_id, name, highway, fase, cenario, lon = x, lat = y) %>%
+  setDT()
+
+# open points - these will be the destination
+points <- fread("../../data/smtr_malha_cicloviaria/r5/graphs/rio/points.csv")
+
 # setup network
 setup <- r5r::setup_r5("../../data/smtr_malha_cicloviaria/r5/graphs/rio")
 
-# open points
-points <- fread("../../data/smtr_malha_cicloviaria/r5/graphs/rio/points.csv")
-
 # calculate ttmatrix
 ttmatrix_bike <- r5r::travel_time_matrix(r5r_core = setup,
-                                    origins = points,
-                                    destinations = points,
-                                    mode = "BICYCLE",
-                                    max_trip_duration = 5)
+                                         origins = cenario_points_coords,
+                                         destinations = points,
+                                         mode = "WALK",
+                                         max_trip_duration = 5)
+
+# trazer osm id para a ttmatrix
+
 
 # save
 fwrite(ttmatrix_bike, "../../data/smtr_malha_cicloviaria/ttmatrix_iso_bike.csv")
